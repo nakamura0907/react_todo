5931812dc8ec5c434e2d5d04a4a03e3ce76d5278
ampan0907 <1801010814fx@cyber-u.ac.jp>
2020-04-13 16:40:13 +0900

    webpack-dev-serverでreact-routerアクセスが可能になった

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -9,7 +9,7 @@ export default class Nav extends React.Component {
                 <ul className="nav-list l-flex">
                     <li className="nav-list-item"><Link to="/">Homeへ</Link></li>
                     <li className="nav-list-item"><Link to="/todo">TodoListへ</Link></li>
-                    <li className="nav-list-item"><Link to="/todo">TodoListへ</Link></li>
+                    <li className="nav-list-item"><Link to="/">随時追加</Link></li>
                 </ul>
             </>
         );

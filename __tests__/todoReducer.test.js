9f103a32c134fcd99ba6d484436ab135ebfa4e3a
ampan0907 <1801010814fx@cyber-u.ac.jp>
2020-04-25 19:40:07 +0900

    reducerのテストができるようになったが、ファイルが大きくなってしまった。 addTodo()のidはランダム作成なのでどのように記述するべきなのか分からない

--- __tests__/todoReducer.test.js
+++ __tests__/todoReducer.test.js
@@ -1,8 +1,8 @@
 import { Actions } from "../src/modules/todoReducer";
 import rootReducer from "../src/modules/rootReducer";
 
-describe("todoReducer", () => {
-  // actions-test
+// actions-test
+describe("todoReducer-Actions", () => {
   it("actions", () => {
     const text = "test-text";
 
@@ -27,20 +27,119 @@ describe("todoReducer", () => {
     };
     expect(Actions.completedTodo(text)).toEqual(expectedAction);
   });
+});
 
-  // reducers-test
-  it("reducers", () => {
+// reducers-test
+describe("todoReducer-reducers", () => {
+  it("initialize", () => {
     expect(rootReducer(undefined, {})).toEqual({
       todo: {
         todos: [],
         value: "",
       },
     });
+  });
 
-    expect(rootReducer(undefined, {})).toEqual({
+  // it("addTodo", () => {
+  //   expect(rootReducer({ todo: { todos: [], value: "Jest" } }, { type: "ADD_TODO", payload: "Jest" })).toEqual({
+  //     todo: {
+  //       todos: [
+  //         {
+  //           id: ?,
+  //           value: "Jest",
+  //           isCompleted: true,
+  //         },
+  //       ],
+  //       value: "",
+  //     },
+  //   });
+  // });
+
+  it("changeValue", () => {
+    expect(rootReducer({ todo: { todos: [], value: "Jes" } }, { type: "CHANGE_VALUE", payload: "Jest" })).toEqual({
       todo: {
         todos: [],
-        value: "",
+        value: "Jest",
+      },
+    });
+
+    expect(rootReducer({ todo: { todos: [], value: "Jest" } }, { type: "CHANGE_VALUE", payload: "Jes" })).toEqual({
+      todo: {
+        todos: [],
+        value: "Jes",
+      },
+    });
+  });
+
+  it("removeTodo", () => {
+    expect(rootReducer({ todo: { todos: [{ id: 0, value: "Jest" }] } }, { type: "REMOVE_TODO", payload: 0 })).toEqual({
+      todo: {
+        todos: [],
+      },
+    });
+
+    expect(
+      rootReducer(
+        {
+          todo: {
+            todos: [
+              { id: 0, value: "Jest" },
+              { id: 1, value: "Jest1" },
+            ],
+          },
+        },
+        { type: "REMOVE_TODO", payload: 1 }
+      )
+    ).toEqual({
+      todo: {
+        todos: [
+          {
+            id: 0,
+            value: "Jest",
+          },
+        ],
+      },
+    });
+  });
+
+  it("completedTodo", () => {
+    expect(
+      rootReducer({ todo: { todos: [{ id: 0, isCompleted: true }] } }, { type: "COMPLETED_TODO", payload: 0 })
+    ).toEqual({
+      todo: {
+        todos: [
+          {
+            id: 0,
+            isCompleted: false,
+          },
+        ],
+      },
+    });
+
+    expect(
+      rootReducer(
+        {
+          todo: {
+            todos: [
+              { id: 0, isCompleted: true },
+              { id: 1, isCompleted: false },
+            ],
+          },
+        },
+        { type: "COMPLETED_TODO", payload: 1 }
+      )
+    ).toEqual({
+      todo: {
+        todos: [
+          {
+            id: 0,
+            isCompleted: true,
+          },
+          {
+            id: 1,
+            isCompleted: true,
+          },
+        ],
       },
     });
   });

a5407cffe7f433a64dad42a1b28602608fb77de0
ampan0907 <1801010814fx@cyber-u.ac.jp>
2020-04-29 18:37:39 +0900

    Todo編集機能の実装。しかし、0から入力する必要があるという問題もある。 テキストをクリックするとフォームに文字列が表示されるようにする?(ChangeTodo?)

--- src/containers/TodoListContainer.tsx
+++ src/containers/TodoListContainer.tsx
@@ -5,12 +5,14 @@ import TodoList from "../components/molecules/TodoList";
 const mapStateToProps = (state) => {
   return {
     todos: state.todo.todos,
+    value: state.form.todoForm.values.todoForm,
   };
 };
 
 const mapDispatchToProps = (dispatch) => ({
   removeTodoFunction: (index): Record<string, number> => dispatch(Actions.removeTodo(index)),
   completedTodoFunction: (id): Record<string, string> => dispatch(Actions.completedTodo(id)),
+  updateTodoFunction: (id, value): Record<string, string> => dispatch(Actions.updateTodo(id, value)),
 });
 
 const TodoListContainer = connect(mapStateToProps, mapDispatchToProps)(TodoList);

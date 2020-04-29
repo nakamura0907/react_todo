ab0f19d034d44091fcc3aaef181e674907cf5338
ampan0907 <1801010814fx@cyber-u.ac.jp>
2020-04-30 00:38:52 +0900

    React-Native（インストールのみ）とElectronの導入

--- webpack.config.js
+++ webpack.config.js
@@ -5,64 +5,78 @@ const MODE = "development";
 // const MODE = "production";
 const enabledSourceMap = MODE === "development";
 
-module.exports = {
-  mode: MODE,
-  entry: "./src/index.tsx",
-  output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js",
+module.exports = [
+  {
+    mode: MODE,
+    entry: {
+      main: "./src/electron/index.js",
+    },
+    output: {
+      path: path.join(__dirname, "public/electron"),
+      filename: "index.js",
+    },
+    module: {},
+    target: "electron-renderer",
   },
-  module: {
-    rules: [
-      {
-        enforce: "pre",
-        test: /\.tsx$/,
-        exclude: /node_modules/,
-        loader: "eslint-loader",
-        options: { fix: true },
-      },
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-      },
-      {
-        test: [/\.sass/, /\.scss/],
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              url: false,
-              sourceMap: enabledSourceMap,
-              importLoaders: 2,
+  {
+    mode: MODE,
+    entry: "./src/index.tsx",
+    output: {
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js",
+    },
+    module: {
+      rules: [
+        {
+          enforce: "pre",
+          test: /\.tsx$/,
+          exclude: /node_modules/,
+          loader: "eslint-loader",
+          options: { fix: true },
+        },
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+        },
+        {
+          test: [/\.sass/, /\.scss/],
+          use: [
+            "style-loader",
+            {
+              loader: "css-loader",
+              options: {
+                url: false,
+                sourceMap: enabledSourceMap,
+                importLoaders: 2,
+              },
             },
-          },
-          {
-            loader: "sass-loader",
-            options: {
-              sourceMap: enabledSourceMap,
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: enabledSourceMap,
+              },
             },
-          },
-        ],
-      }, // css
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-            options: { minimize: true },
-          },
-        ],
-      }, // html
+          ],
+        }, // css
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader",
+              options: { minimize: true },
+            },
+          ],
+        }, // html
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js", ".json"],
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html",
+      }),
     ],
   },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js", ".json"],
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html",
-    }),
-  ],
-};
+];

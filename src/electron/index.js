c5e4d2299f79449b68342d5cd1cffe93a67a7ccf
ampan0907 <1801010814fx@cyber-u.ac.jp>
2020-05-03 19:44:08 +0900

    electronに設定メニューを追加した。README.mdの番号付きリストの不具合を解消した

--- src/electron/index.js
+++ src/electron/index.js
@@ -1,10 +1,59 @@
-import { app, BrowserWindow } from "electron";
-
-// import path from "path";
-// const ROOT_PATH = "file://" + path.resolve("");
-// const rootPath = `${ROOT_PATH}/public/index.html`;
+import { app, Menu, BrowserWindow } from "electron";
 
 app.on("ready", (e) => {
+  const template = [
+    {
+      label: "編集",
+      submenu: [
+        {
+          label: "戻る",
+          accelerator: "Ctrl+Z",
+          role: "undo",
+        },
+        {
+          label: "進む",
+          accelerator: "Ctrl+Y",
+          role: "redo",
+        },
+        {
+          label: "コピー",
+          accelerator: "Ctrl+C",
+          role: "copy",
+        },
+        {
+          label: "貼り付け",
+          accelerator: "Ctrl+V",
+          role: "paste",
+        },
+      ],
+    }, // 編集
+    {
+      label: "表示",
+      submenu: [
+        {
+          label: "ウィンドウ最小化",
+          role: "minimize",
+        },
+        {
+          label: "ウィンドウを閉じる",
+          role: "close",
+        },
+        {
+          label: "フルスクリーン",
+          role: "togglefullscreen",
+        },
+        {
+          label: "再読み込み",
+          accelerator: "F5",
+          role: "reload",
+        },
+      ],
+    }, // 表示
+  ];
+
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+
   const windSetting = { window: 800, height: 600 };
   const mainWindow = new BrowserWindow(windSetting);
   mainWindow.loadFile("public/index.html");

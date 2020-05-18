f5dedffec29122e36fb89642e60711a56fc534d2
ampan0907 <1801010814fx@cyber-u.ac.jp>
2020-05-18 22:17:53 +0900

    テストの作成

--- __tests__/todoReducer.test.js
+++ __tests__/todoReducer.test.js
@@ -1,25 +1,248 @@
 import { Actions } from "../src/modules/todo";
-import reducers from "../src/modules";
+import reducer from "../src/modules";
 
 // actions-test
 describe("todoReducer-Actions", () => {
   it("actions", () => {
-    const text = "test-text";
+    const arg1 = "hoge";
+    const arg2 = "hoge2";
+    let expectedAction;
 
-    let expectedAction = {
-      type: "ADD_TODO",
-      payload: text,
-    };
-    expect(Actions.addTodo(text)).toEqual(expectedAction);
+    // expectedAction = {
+    //   type: "ADD_TODO",
+    //   payload: {
+    //     id: arg1,
+    //     value: arg1,
+    //     isCompleted: false,
+    //     isForm: false,
+    //   },
+    // };
+    // expect(Actions.addTodo(arg1)).toEqual(expectedAction);
     expectedAction = {
       type: "REMOVE_TODO",
-      payload: text,
+      payload: {
+        index: arg1,
+      },
     };
-    expect(Actions.removeTodo(text)).toEqual(expectedAction);
+    expect(Actions.removeTodo(arg1)).toEqual(expectedAction);
     expectedAction = {
       type: "COMPLETED_TODO",
-      payload: text,
+      payload: {
+        id: arg1,
+      },
     };
-    expect(Actions.completedTodo(text)).toEqual(expectedAction);
+    expect(Actions.completedTodo(arg1)).toEqual(expectedAction);
+    expectedAction = {
+      type: "UPDATE_TODO",
+      payload: {
+        id: arg1,
+        value: arg2,
+      },
+    };
+    expect(Actions.updateTodo(arg1, arg2)).toEqual(expectedAction);
+    expectedAction = {
+      type: "CHANGE_TEXTFORM",
+      payload: {
+        id: arg1,
+      },
+    };
+    expect(Actions.changeTextform(arg1)).toEqual(expectedAction);
+  });
+});
+
+describe("todoReducer-reducers", () => {
+  const INITIAL_STATE = {
+    form: {},
+    todo: {
+      todos: [],
+    },
+  };
+
+  it("initialize", () => {
+    expect(reducer(undefined, { type: null })).toEqual(INITIAL_STATE);
+  });
+
+  it("addTodo", () => {});
+
+  it("removeTodo", () => {});
+
+  it("completedTodo", () => {
+    expect(
+      reducer(
+        {
+          todo: {
+            todos: [
+              {
+                id: 1,
+                isCompleted: false,
+              },
+            ],
+          },
+        },
+        { type: "COMPLETED_TODO", payload: { id: 1 } }
+      )
+    ).toEqual({
+      form: {},
+      todo: {
+        todos: [
+          {
+            id: 1,
+            isCompleted: true,
+          },
+        ],
+      },
+    });
+
+    expect(
+      reducer(
+        {
+          todo: {
+            todos: [
+              {
+                id: 1,
+                isCompleted: true,
+              },
+            ],
+          },
+        },
+        { type: "COMPLETED_TODO", payload: { id: 1 } }
+      )
+    ).toEqual({
+      form: {},
+      todo: {
+        todos: [
+          {
+            id: 1,
+            isCompleted: false,
+          },
+        ],
+      },
+    });
+  });
+
+  it("updateTodo", () => {
+    expect(
+      reducer(
+        {
+          todo: {
+            todos: [
+              {
+                id: 1,
+                value: "hogee",
+                isForm: true,
+              },
+            ],
+          },
+        },
+        {
+          type: "UPDATE_TODO",
+          payload: {
+            id: 1,
+            value: "hoge",
+          },
+        }
+      )
+    ).toEqual({
+      form: {},
+      todo: {
+        todos: [
+          {
+            id: 1,
+            isForm: false,
+            value: "hoge",
+          },
+        ],
+      },
+    });
+  });
+
+  it("changeTextform", () => {
+    expect(
+      reducer(
+        {
+          todo: {
+            todos: [
+              {
+                id: 1,
+                isForm: false,
+              },
+              {
+                id: 2,
+                isForm: false,
+              },
+            ],
+          },
+        },
+        {
+          type: "CHANGE_TEXTFORM",
+          payload: {
+            id: 2,
+          },
+        }
+      )
+    ).toEqual({
+      form: {},
+      todo: {
+        todos: [
+          {
+            id: 1,
+            isForm: false,
+          },
+          {
+            id: 2,
+            isForm: true,
+          },
+        ],
+      },
+    });
+  });
+
+  it("chancelUpdate", () => {
+    expect(
+      reducer(
+        {
+          todo: {
+            todos: [
+              {
+                id: 1,
+                isForm: false,
+              },
+              {
+                id: 2,
+                isForm: true,
+              },
+              {
+                id: 3,
+                isForm: false,
+              },
+            ],
+          },
+        },
+        {
+          type: "CHANCEL_UPDATE",
+          payload: {
+            id: 2,
+          },
+        }
+      )
+    ).toEqual({
+      form: {},
+      todo: {
+        todos: [
+          {
+            id: 1,
+            isForm: false,
+          },
+          {
+            id: 2,
+            isForm: false,
+          },
+          {
+            id: 3,
+            isForm: false,
+          },
+        ],
+      },
+    });
   });
 });
